f69f4853f7637ca3524c131d2cef9b7e835af334
Renato-Barreto <143633174+Renato-Barreto@users.noreply.github.com>
2023-11-28 16:08:24 -0300

    Update Atv4P.c

--- Atv4P.c
+++ Atv4P.c
@@ -1,9 +1,9 @@
-//Crie um software que leia 5 números inteiros e, em seguida, mostre na tela:
-//A quantidade de números ímpares;
-//A quantidade de números negativos;
-//O maior e o menor número;
-//A média de números pares;
-//A média de números inseridos.
+//Crie um software que leia 5 nÃºmeros inteiros e, em seguida, mostre na tela:
+//A quantidade de nÃºmeros Ã­mpares;
+//A quantidade de nÃºmeros negativos;
+//O maior e o menor nÃºmero;
+//A mÃ©dia de nÃºmeros pares;
+//A mÃ©dia de nÃºmeros inseridos.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,23 +14,23 @@
 int main () {
     setlocale(LC_ALL,"portuguese");
 
-    //Declarando Variáveis
+    //Declarando VariÃ¡veis
     int i = 0;
     int tamanho = 5;
     int numero[tamanho];
     int quantidadePares = 0, quantidadeImpares = 0;
-    int totalPares = 0, totalImpares = 0;
+    int totalPares = 0;
     int quantidadeNegativos = 0;
     int totalNegativos = 0;
     int maior = 0, menor = 99999;
     int total = 0, quantidadeTotal = 0;
     float mediaPar, mediaTotal;
 
-    //Solicitando Números
-    printf("Solicitando os 5 números inteiros abaixo\n");
+    //Solicitando NÃºmeros
+    printf("Solicitando os 5 nÃºmeros inteiros abaixo\n");
     for ( i = 0; i < tamanho; i++)
     {
-        printf("Digite o %iº número: ",i+1);
+        printf("Digite o %iÂº nÃºmero: ",i+1);
         scanf("%d",&numero[i]);
 
         //Trabalhando com Pares e Impares
@@ -46,7 +46,6 @@ int main () {
         if (numero[i] < 0)
         {
             quantidadeNegativos++;
-            totalNegativos += numero[i];
         }
 
         //Maior e Menor
@@ -57,7 +56,7 @@ int main () {
         total += numero[i];
         quantidadeTotal ++;
     }
-    //Média
+    //MÃ©dia
     if (quantidadePares != 0) {
         mediaPar = totalPares / quantidadePares;
     }
@@ -65,16 +64,16 @@ int main () {
     mediaTotal = total / quantidadeTotal;
     system("cls || clear");
 
-    //Mostrando para o usuário
-    printf("Quantidade de números Ímpares: %i\n",quantidadeImpares);
+    //Mostrando para o usuÃ¡rio
+    printf("Quantidade de nÃºmeros Ãmpares: %i\n",quantidadeImpares);
     printf("\n");
-    printf("Quantidade de números Negativos: %i\n",quantidadeNegativos);
+    printf("Quantidade de nÃºmeros Negativos: %i\n",quantidadeNegativos);
     printf("\n");
-    printf("Maior número: %i\n",maior);
-    printf("Menor número: %i\n",menor);
+    printf("Maior nÃºmero: %i\n",maior);
+    printf("Menor nÃºmero: %i\n",menor);
     printf("\n");
-    printf("Média de números pares: %.2f\n",mediaPar);
-    printf("Média Total: %.2f\n",mediaTotal);
+    printf("MÃ©dia de nÃºmeros pares: %.2f\n",mediaPar);
+    printf("MÃ©dia Total: %.2f\n",mediaTotal);
 
     return 0;
 }
